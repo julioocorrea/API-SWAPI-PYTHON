7ed5229e64952a28c385791ae570a263b8da4e2d
Júlio Correa <111064913+julioocorrea@users.noreply.github.com>
2024-10-07 09:05:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ SWAPI_BASE_URLS = {
     'films': 'https://swapi.dev/api/films/',
     'starships': 'https://swapi.dev/api/starships/',
     'vehicles': 'https://swapi.dev/api/vehicles/',
-    'species': 'https://swapi.dev/api/species/'
+    'species': 'https://swapi.dev/api/species/',
+    'planets': 'https://swapi.dev/api/planets/',
 }
 
 async def fetch_data(session, url):
@@ -175,6 +176,114 @@ async def get_species_by_id(species_index):
             species_data['homeworld_name'] = 'Desconhecido'
 
         return render_template('species_detail.html', species=species_data, species_index=species_index)
+
+@app.route('/planets', methods=['GET'])
+@cache.cached(timeout=120)  # Cache por 60 segundos
+async def get_planets():
+    async with aiohttp.ClientSession() as session:
+        data = await fetch_all_data(session, [SWAPI_BASE_URLS['planets']])
+        planets = data[0]['results']
+        return render_template('planets.html', planets=planets)
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+@cache.cached(timeout=60)  # Cache por 60 segundos
+async def get_planet(planet_id):
+    async with aiohttp.ClientSession() as session:
+        planet_data = await fetch_data(session, f"{SWAPI_BASE_URLS['planets']}{planet_id}/")
+
+        # Verificar habitantes
+        inhabitants = []
+        for character_url in planet_data.get('residents', []):
+            character_data = await fetch_data(session, character_url)
+            inhabitants.append(character_data.get('name', 'Desconhecido'))
+
+        planet_data['inhabitants'] = inhabitants
+
+        return render_template('planet_detail.html', planet=planet_data, planet_index=planet_id)
+    
+@app.route('/favorites', methods=['GET'])
+async def get_favorites():
+    connection = create_connection()
+    favorites = []
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT id, item_index, item_type FROM favorites")
+            rows = cursor.fetchall()
+            for favorite_id, item_index, item_type in rows:
+                favorites.append({'id': favorite_id, 'item_index': item_index, 'item_type': item_type})
+        except Error as e:
+            print(f"Erro ao buscar favoritos: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    return render_template('favorites.html', favorites=favorites)
+
+@app.route('/add_favorite', methods=['POST'])
+def add_favorite():
+    print("Requisição recebida para adicionar favorito")  # Log
+    data = request.get_json()
+    print(f"Dados recebidos: {data}")  # Log
+    
+    if not data:
+        print("Dados não recebidos.")
+        return jsonify({"error": "Dados não recebidos."}), 400
+    
+    item_type = data.get('type')
+    item_index = data.get('index')
+
+    if not item_type or not item_index:
+        print("Tipo ou índice do item não fornecido.")
+        return jsonify({"error": "Tipo ou índice do item não fornecido."}), 400
+
+    connection = create_connection()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            
+            # Verificar se o registro já existe
+            cursor.execute("SELECT COUNT(*) FROM favorites WHERE item_type = %s AND item_index = %s", (item_type, item_index))
+            count = cursor.fetchone()[0]
+            
+            if count > 0:
+                print("Este item já está salvo como favorito.")
+                return jsonify({"error": "Este item já está salvo como favorito."}), 409  # 409 Conflict
+            
+            # Inserir novo favorito
+            query = "INSERT INTO favorites (item_index, item_type) VALUES (%s, %s)"
+            cursor.execute(query, (item_index, item_type))
+            connection.commit()
+            print("Item adicionado com sucesso aos favoritos!")  # Log de sucesso
+            return jsonify({"message": "Item adicionado aos favoritos!"}), 200
+        except Error as e:
+            print(f"Erro ao inserir no banco de dados: {e}")
+            return jsonify({"error": "Erro ao adicionar ao banco de dados."}), 500
+        finally:
+            cursor.close()
+            connection.close()
+    
+    print("Falha na conexão com o banco de dados.")
+    return jsonify({"error": "Falha na conexão com o banco de dados."}), 500
+
+
+@app.route('/favorites/delete/<int:item_id>', methods=['POST'])
+def delete_favorite(item_id):
+    connection = create_connection()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            # Usar o id para deletar o registro
+            cursor.execute("DELETE FROM favorites WHERE id = %s", (item_id,))
+            connection.commit()
+            print(f"Item com id {item_id} deletado com sucesso!")  # Log de sucesso
+        except Error as e:
+            print(f"Erro ao deletar do banco de dados: {e}")
+            return jsonify({"error": "Erro ao deletar do banco de dados."}), 500
+        finally:
+            cursor.close()
+            connection.close()
+    
+    return redirect('/favorites')  # Redireciona para a página de favoritos
 
 if __name__ == '__main__':
     app.run(debug=True)
